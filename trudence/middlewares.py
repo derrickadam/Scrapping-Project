58b72e82c6615ad22a393aa62aadd0007511fdcb
Abdelwahab <fa_chibah@esi.dz>
2020-03-17 17:22:35 +0100

    removed selenium spider

--- trudence/middlewares.py
+++ trudence/middlewares.py
@@ -6,12 +6,6 @@
 # https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 import random
 
-from scrapy import signals
-from scrapy.http import HtmlResponse
-from scrapy.utils.python import to_bytes
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-
 
 class CustomProxyMiddleware(object):
     def process_request(self, request, spider):
@@ -19,50 +13,3 @@ class CustomProxyMiddleware(object):
             proxy = random.choice(spider.proxy_pool)
             request.meta['proxy'] = "http://" + str(proxy)
             print("New proxy  for ", request.url, proxy)
-
-
-class SeleniumMiddleware(object):
-    @classmethod
-    def from_crawler(cls, crawler):
-        middleware = cls()
-        crawler.signals.connect(middleware.spider_opened, signals.spider_opened)
-        crawler.signals.connect(middleware.spider_closed, signals.spider_closed)
-        return middleware
-
-    def process_request(self, request, spider):
-        proxy = ""
-        if spider.use_proxy:
-            proxy = random.choice(spider.proxy_pool)
-            spider.logger.warning(f"New proxy {proxy} for {request.url}")
-        self.driver = open_driver(proxy, spider.use_proxy)
-        self.driver.get(request.url)
-
-        body = to_bytes(self.driver.page_source)  # body must be of type bytes
-        response = HtmlResponse(self.driver.current_url, body=body, encoding='utf-8', request=request)
-        for i in spider.bad_texts:
-            if i in response.text or len(self.driver.page_source) < 10:
-                spider.logger.warning(f"Wrong content for {request.url}")
-                return None
-        self.driver.close()
-        return response
-
-    def spider_opened(self, spider):
-        pass
-
-    def spider_closed(self, spider):
-        pass
-
-
-def open_driver(proxy, use_proxy):
-    chrome_options = Options()
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--disable-infobars")
-    if use_proxy:
-        chrome_options.add_argument('--proxy-server=%s' % proxy)
-    driver = webdriver.Chrome(chrome_options=chrome_options)
-    driver.set_page_load_timeout(5)
-    return driver
--- trudence/spiders/seleniumspider.py
+++ trudence/spiders/seleniumspider.py
@@ -1,24 +1,24 @@
-# -*- coding: utf-8 -*-
-
-from trudence.spiders.proxyspider import ProxySpider
-
-
-class SeleniumspiderSpider(ProxySpider):
-    name = 'seleniumspider'
-    custom_settings = {
-        'DOWNLOADER_MIDDLEWARES': {
-            'trudence.middlewares.SeleniumMiddleware': 10,
-            'scrapy.downloadermiddlewares.retry.RetryMiddleware': 100,
-            'trudence.middlewares.CustomProxyMiddleware': 350,
-            'scrapy.downloadermiddlewares.httpproxy.HttpProxyMiddleware': 400,
-        },
-        "CONCURRENT_REQUESTS": 1,
-        'CONCURRENT_REQUESTS_PER_DOMAIN': 1,
-        'CONCURRENT_REQUESTS_PER_IP': 1
-    }
-    run_next_enabled = False
-    bad_texts = ["The requested URL could not be retrieved", "The service is unavailable", "Access Denied"]
-
-    def __init__(self, is_master=True, lower_bound=0, use_proxy=False, **kwargs):
-        super().__init__(is_master=True, lower_bound=0, **kwargs)
-        self.use_proxy = (use_proxy is not False)
+# # -*- coding: utf-8 -*-
+#
+# from trudence.spiders.proxyspider import ProxySpider
+#
+#
+# class SeleniumspiderSpider(ProxySpider):
+#     name = 'seleniumspider'
+#     custom_settings = {
+#         'DOWNLOADER_MIDDLEWARES': {
+#             'trudence.middlewares.SeleniumMiddleware': 10,
+#             'scrapy.downloadermiddlewares.retry.RetryMiddleware': 100,
+#             'trudence.middlewares.CustomProxyMiddleware': 350,
+#             'scrapy.downloadermiddlewares.httpproxy.HttpProxyMiddleware': 400,
+#         },
+#         "CONCURRENT_REQUESTS": 1,
+#         'CONCURRENT_REQUESTS_PER_DOMAIN': 1,
+#         'CONCURRENT_REQUESTS_PER_IP': 1
+#     }
+#     run_next_enabled = False
+#     bad_texts = ["The requested URL could not be retrieved", "The service is unavailable", "Access Denied"]
+#
+#     def __init__(self, is_master=True, lower_bound=0, use_proxy=False, **kwargs):
+#         super().__init__(is_master=True, lower_bound=0, **kwargs)
+#         self.use_proxy = (use_proxy is not False)
